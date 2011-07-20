d11242c7d6e5b59bec6f82031090c72506d3d29f
Gary L. Hines <glhines@acm.org>
2011-07-20 15:19:51 -0400

    Done with sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
+      sign_in @user # Automatically sign new user into a Session
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
